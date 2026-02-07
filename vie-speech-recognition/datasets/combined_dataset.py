ebdc484ef2596106c3effb304a9171f0225d8c90
admin <nguyenquochuydl123@gmail.com>
2026-02-07 15:04:39 +0700

    Remove merge vie-train

--- vie-speech-recognition/datasets/combined_dataset.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from .dataset_factory import DatasetFactory
-
-class CombinedDatasetFactory(DatasetFactory):
-    def create_df(self):
-        return pd.read_csv('./filtered_dataset.csv')
-
--- vie-speech-recognition/models/vie_wav2vec_model.py
+++
@@ -1,6 +0,0 @@
-from models.base_model import BaseModel
-
-
-class VieWavToVecModel(BaseModel):
-    def __init__(self, pretrained_path, special_tokens):
-        super().__init__(pretrained_path, special_tokens)
